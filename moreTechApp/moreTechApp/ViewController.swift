261a848b4bae3f11316acfdf2e594a762b5976aa
Ibragim <ibragim.m115@gmail.com>
2021-10-08 16:28:07 +0300

    Project init

---
+++ moreTechApp/moreTechApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moreTechApp
+//
+//  Created by i.mamadaev on 08.10.2021.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
